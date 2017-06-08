3f2c903ff02b5b05c04bb061d068cc14ebe801c4
sablanchard <blanchard@urbansim.com>
2017-06-08 16:26:30 -0700

    updated plotting, pep8 formatting, missing docstring,  and ValueError refactor

--- urbanaccess/plot.py
+++ urbanaccess/plot.py
@@ -7,12 +7,13 @@ import time
 
 from urbanaccess.utils import log
 
-def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
-             to_col='to_int', bbox=None,
-             fig_height=6,margin=0.02,
-             edge_color='#999999',edge_linewidth=1,edge_alpha=1,
-             node_color='black',node_size=15,node_alpha=1,
-             node_edgecolor='none',node_zorder=3,nodes_only=False):
+
+def plot_net(nodes, edges, x_col=None, y_col=None, from_col=None,
+             to_col=None, bbox=None,
+             fig_height=6, margin=0.02,
+             edge_color='#999999', edge_linewidth=1, edge_alpha=1,
+             node_color='black', node_size=15, node_alpha=1,
+             node_edgecolor='none', node_zorder=3, nodes_only=False):
     """
     plot urbanaccess network nodes and edges
 
@@ -66,10 +67,29 @@ def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
 
     start_time = time.time()
 
+    # set default x and y cols if none specified
+    if 'x' in nodes.columns and 'y' in nodes.columns and x_col is \
+            None and y_col is None:
+        x_col = 'x'
+        y_col = 'y'
+
+    # set default from_col and to_col cols if none specified
+    if 'from_int' in edges.columns and 'to_int' in edges.columns and\
+                    from_col is None and to_col is None:
+        from_col = 'from_int'
+        to_col = 'to_int'
+
+    # set default from_col and to_col cols if none specified
+    if 'node_id_from' in edges.columns and 'node_id_to' in edges.columns and\
+                    from_col is None and to_col is None:
+        from_col = 'node_id_from'
+        to_col = 'node_id_to'
+
+
     # if edge df is subset make sure nodes are also subset to match
     from_ids = nodes[nodes.index.isin(list(edges[from_col]))]
     to_id = nodes[nodes.index.isin(list(edges[to_col]))]
-    node_ids = pd.concat([from_ids,to_id],ignore_index=False)
+    node_ids = pd.concat([from_ids, to_id], ignore_index=False)
     nodes = nodes[nodes.index.isin(list(node_ids.index))]
 
     node_Xs = nodes[x_col].tolist()
@@ -80,6 +100,7 @@ def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
                 or 'to_lon' not in edges.columns \
                 or 'from_lat' not in edges.columns \
                 or 'from_lon' not in edges.columns:
+
             edges = _prep_edges(edges=edges, nodes=nodes,
                                 from_col=from_col, to_col=to_col,
                                 x_col=x_col, y_col=y_col)
@@ -92,15 +113,17 @@ def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
     else:
         x_min, y_min, x_max, y_max = bbox
 
-    assert y_max-y_min >= 0 and x_max-x_min >= 0,\
-        'difference between min and max x and y resulted in a negative value'
-    bbox_aspect_ratio = (y_max-y_min)/(x_max-x_min)
+    if y_max - y_min <= 0 and x_max - x_min <= 0:
+        raise ValueError(('difference between min and max x and y resulted '
+                          'in a negative value or 0'))
+    bbox_aspect_ratio = (y_max - y_min) / (x_max - x_min)
 
     fig, ax = plt.subplots(figsize=(fig_height / bbox_aspect_ratio,
                                     fig_height))
 
     if nodes_only == False:
-        #TODO: optimize for speed by calculating only for edges that are within the the bbox + buffer distance to speed up
+        # TODO: optimize for speed by calculating only for edges that are
+        # within the the bbox + buffer distance to speed up
         lines = []
         for index, node in edges.iterrows():
             x1, y1, x2, y2 = node['from_lon'], node['from_lat'], \
@@ -118,8 +141,8 @@ def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
                zorder=node_zorder)
 
     # set fig extent
-    margin_ns = (y_min-y_max) * margin
-    margin_ew = (x_min-x_max) * margin
+    margin_ns = (y_min - y_max) * margin
+    margin_ew = (x_min - x_max) * margin
     ax.set_ylim((y_min - margin_ns, y_max + margin_ns))
     ax.set_xlim((x_min - margin_ew, x_max + margin_ew))
 
@@ -128,14 +151,15 @@ def plot_net(nodes=None,edges=None,x_col='x',y_col='y', from_col='from_int',
     ax.get_yaxis().get_major_formatter().set_useOffset(False)
     ax.axis('off')
 
-    log('Figure created. Took {:,.2f} seconds'.format(time.time()-start_time))
+    log('Figure created. Took {:,.2f} seconds'.format(
+        time.time() - start_time))
 
     plt.show()
 
     return fig, ax
 
 
-def col_colors(df=None, col=None, num_bins=5, cmap='spectral',
+def col_colors(df, col, num_bins=5, cmap='spectral',
                start=0.1, stop=0.9):
     """
     Get a list of colors by binning a continuous variable column
@@ -165,7 +189,7 @@ def col_colors(df=None, col=None, num_bins=5, cmap='spectral',
     if not bins_used == num_bins:
         log('Too many bins requested, using max bins possible. '
             'To avoid duplicate edges, ' + str(bins_used) + ' bins used.')
-    
+
     color_list = [cm.get_cmap(cmap)(x) for x in np.linspace(start,
                                                             stop,
                                                             bins_used)]
@@ -175,17 +199,33 @@ def col_colors(df=None, col=None, num_bins=5, cmap='spectral',
 
 
 def _recursive_category_gen(col, num_bins):
+    """
+    Generate number of bins recursively
+
+    Parameters
+    ----------
+    col : string
+        the name of the column in the dataframe with the continuous variable
+    num_bins : int
+        how many quantiles
+
+    Returns
+    -------
+    num_bins : int
+    categories : list
+    """
+
     bin_labels = range(num_bins)
 
     # base case catch
     if num_bins == 0:
         raise ValueError('Unable to perform qcut to 0 bins.')
-    
+
     # we assume the num_bins count will work
     try:
         categories = pd.qcut(x=col, q=num_bins, labels=bin_labels)
         return num_bins, categories
-    
+
     # if it does not, then we need to go down 1 number of bins
     except ValueError:
         new_bin_count = num_bins - 1
@@ -216,16 +256,22 @@ def _prep_edges(edges, nodes, from_col, to_col,
         the edge dataframe with from and to x y coordinates and
         ids to build lines
     """
-    assert x_col in nodes.columns and y_col in nodes.columns, \
-        'x or y column was not found in node columns: {}'
-    assert from_col in edges.columns and to_col in edges.columns, \
-        'from or to column was not found in edge columns: {}'
 
-    edges_wline = edges.merge(nodes[[x_col,y_col]], left_on=from_col,
+    if x_col not in nodes.columns and y_col not in nodes.columns:
+        raise ValueError(
+            '{} or {} columns were not found in nodes columns'.format(x_col,
+                                                                      y_col))
+
+    if from_col not in nodes.columns and to_col not in edges.columns:
+        raise ValueError(
+            '{} or {} columns were not found in edges columns'.format(from_col,
+                                                                      to_col))
+
+    edges_wline = edges.merge(nodes[[x_col, y_col]], left_on=from_col,
                               right_index=True)
     edges_wline.rename(columns={x_col: 'from_lon', y_col: 'from_lat'},
                        inplace=True)
-    edges_wline = edges_wline.merge(nodes[[x_col,y_col]], left_on=to_col,
+    edges_wline = edges_wline.merge(nodes[[x_col, y_col]], left_on=to_col,
                                     right_index=True)
     edges_wline.rename(columns={x_col: 'to_lon', y_col: 'to_lat'},
                        inplace=True)
