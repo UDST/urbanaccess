1208c9b547b5d98ae416e082d491090e43f7df65
sablanchard <blanchard@urbansim.com>
2017-09-21 10:51:05 -0700

    revert changes to readme, updated setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ try:
     import setuptools
 except ImportError:
     from ez_setup import use_setuptools
+
     use_setuptools()
 
 from setuptools import setup, find_packages
 
-
 # read README as the long description
-with open('README.md', 'r') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
+
+description = 'A tool for creating GTFS transit and OSM pedestrian networks ' \
+              'for use in Pandana accessibility analyses.'
 
 setup(
     name='urbanaccess',
-    version='0.2',
+    version='0.2.0',
     license='AGPL',
-    description='A tool for creating GTFS transit and OSM pedestrian networks for use in Pandana accessibility analyses.',
+    description=description,
     long_description=long_description,
     author='UrbanSim Inc. and Samuel D. Blanchard',
     url='https://github.com/UDST/urbanaccess',
@@ -28,14 +31,14 @@ setup(
     ],
     packages=find_packages(exclude=['*.tests']),
     install_requires=[
-        'requests==2.9.1',
-        'pandas==0.20.3',
-        'numpy==1.13.1',
-        'osmnet==0.1.4',
-        'pandana==0.4.0.1',
-        'matplotlib==2.0.2',
-        'geopy==1.11.0',
-        'pyyaml==3.11',
-        'scikit-learn==0.18.2'
+        'requests>=2.9.1',
+        'pandas>=0.20.3',
+        'numpy>=1.13.1',
+        'osmnet>=0.1.4',
+        'pandana>=0.3.0',
+        'matplotlib>=2.0.2',
+        'geopy>=1.11.0',
+        'pyyaml>=3.11',
+        'scikit-learn>=0.18.2'
     ]
 )
