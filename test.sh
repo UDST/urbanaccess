a84630b2bd35bd85020d954722cd75b9dc4de6fc
sablanchard <blanchard@urbansim.com>
2017-09-21 10:52:20 -0700

    cleaned up comments

--- test.sh
+++ test.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# I think the correct way to do this is to add these
-# dependencies via 'tests_require' in setup.py. I ran
-# into issue getting scipy installed this way though,
-# so for now these are basically copied from .travis.yml
 pip install \
   future \
   pytest \
   scipy
 
-# Don't depend on the code in the Docker image
-# being up to date.
 pip install .
 
 # Run tests
