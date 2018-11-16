f48ae0e01e142678ed5543540c7c3e4b55224ed1
udst-documentator <fernandez@urbansim.com>
2018-11-16 17:16:49 -0300

    removed quite

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -36,7 +36,7 @@ if [ "$TRAVIS_REPO_SLUG" == "UDST/urbanaccess" ] && \
         [ "$ACTUAL_TRAVIS_JOB_NUMBER" == "1" ]; then
 
         echo "Installing dependencies"
-        conda install --yes --quiet sphinx numpydoc
+        conda install --yes sphinx numpydoc
         pip install sphinx_rtd_theme
 
         echo "Building docs"
